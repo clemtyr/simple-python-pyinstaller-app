2a3482566e6dbac3bc97b1d5139b198ba265a661
Dast <ctyranowicz@gmail.com>
2024-06-27 21:02:47 +0200

    try to commit

--- sources/calc.py
+++ sources/calc.py
@@ -6,7 +6,7 @@ NOTE: If a value submitted to the 'add2' function is a float, it must be done so
 in quotes (i.e. as a string).
 '''
 
-# If 'value' is not an integer, convert it to a float and failing that, a string.
+# If 'value' is not an integer, convert it to a float and failing that, a string
 def conv(value):
     try:
         return int(value)
@@ -21,7 +21,7 @@ def add2(arg1, arg2):
     # Convert 'arg1' and 'arg2' to their appropriate types
     arg1conv = conv(arg1)
     arg2conv = conv(arg2)
-        # If either 'arg1' or 'arg2' is a string, ensure they're both strings.
+        # If either 'arg1' or 'arg2' is a string, ensure they're both strings
     if isinstance(arg1conv, str) or isinstance(arg2conv, str):
         arg1conv = str(arg1conv)
         arg2conv = str(arg2conv)
